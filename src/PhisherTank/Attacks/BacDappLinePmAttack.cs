77a39e1430a85d19c801c30c145644e34b0b3375
César Morgan <xds_xps_ivx@hotmail.com>
2023-12-26 21:01:26 -0600

    New attack: BacDappLinePm

---
+++ src/PhisherTank/Attacks/BacDappLinePmAttack.cs
@@ -0,0 +1,43 @@
+﻿using TheXDS.PhisherTank.Component;
+using TheXDS.PhisherTank.Models;
+
+namespace TheXDS.PhisherTank.Attacks;
+
+internal class BacDappLinePmAttack : Attack
+{
+    public BacDappLinePmAttack() : base("bac-dapp.line.pm")
+    {
+    }
+
+    public override IEnumerable<AttackItem> GetAttacks(IAttackContext context)
+    {
+        yield return "";
+        yield return "pws.php";
+        yield return new("pws.php")
+        {
+            FormItems = f => new[] {
+                ("usuario", f.Person.UserName),
+                ("clave", f.Password),
+                ("submit", "")
+            }
+        };
+        yield return "load.html";
+        yield return "token.php";
+        var pin = MiscFaker.FakePin(6);
+        yield return new("token.php")
+        {
+            FormItems = f => new[] {
+                ("usuario", pin),
+                ("submit", "")
+            }
+        };
+        yield return "errortoken.php";
+        yield return new("errortoken.php")
+        {
+            FormItems = f => new[] {
+                ("usuario", pin),
+                ("submit", "")
+            }
+        };
+    }
+}
