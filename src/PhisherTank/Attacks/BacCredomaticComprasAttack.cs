a09e47e195100472d7e8276bc0584558c86eb49f
César Morgan <xds_xps_ivx@hotmail.com>
2024-01-05 11:19:07 -0600

    New (but incomplete) BacCredomaticComprasAttack

---
+++ src/PhisherTank/Attacks/BacCredomaticComprasAttack.cs
@@ -0,0 +1,59 @@
+﻿using TheXDS.MCART.Types.Extensions;
+using TheXDS.PhisherTank.Component;
+using TheXDS.PhisherTank.Models;
+
+namespace TheXDS.PhisherTank.Attacks;
+
+internal class BacCredomaticComprasAttack() : Attack("baccredomaticcompras.weebly.com")
+{
+    private string GetRedirectButton(HttpResponseMessage response)
+    {
+        return "5367420a-2d4c-42b8-b76b-39c6056f4873-00-lw6fyscf8jox.worf.replit.dev";
+        //int buttonLocation = 328;
+
+        //using var rs = response.Content.ReadAsStream();
+        //using var sr = new StreamReader(rs);
+
+        //while (buttonLocation-- > 0) { var l = sr.ReadLine(); }
+        //return sr.ReadLine()!.ChopStartAny("<a href='https://", "<a href='http://").ChopEnd("' target='_blank'>");
+    }
+
+    public override IEnumerable<AttackItem> GetAttacks(IAttackContext context)
+    {
+        context.AddCommonBrowserHeaders();
+        //yield return "";
+        var newServer = GetRedirectButton(context.LastResponse!);
+        context.SwitchServer(newServer, this);
+        context.Headers.Add("Host", newServer);
+        context.Headers.Add("Origin", $"https://{newServer}");
+        context.Headers.Add("Referer", $"https://{newServer}");
+        yield return "";
+        yield return Form("rgcrgs.php", GetRgcrgs);
+        yield return "oucls.php";
+        yield return Form("oucls2.php", GetOuCls);
+        yield return Form("oucls3.php", GetOuCls);
+        yield return Form("trufs.php", GetOuCls);
+    }
+
+    private (string, string)[] GetRgcrgs(DataBase f)
+    {
+        return [
+            ("country", "HN"),
+            ("caraqueño", f.Person.UserName),
+            ("pequeñiso", f.Password),
+            ("passtemp", ""),
+            ("token", ""),
+            ("signatureDataHash", ""),
+            ("persistent", MiscFaker._rnd.CoinFlip() ? "y":"n"),
+            ("confirm", "")
+        ];
+    }
+
+    private (string, string)[] GetOuCls(DataBase _)
+    {
+        return [
+            ("caraqueñAZO", MiscFaker.FakePin(6)),
+            ("precari0", MiscFaker.FakePin(6)),
+        ];
+    }
+}
