3814ec53fe9361e15e298f6e401865704e489ea8
César Morgan <xds_xps_ivx@hotmail.com>
2024-03-10 01:14:48 -0600

    Cleanup

---
+++ src/PhisherTank/Attacks/CelebratorsPkAttack.cs
@@ -0,0 +1,26 @@
+﻿using TheXDS.MCART.Types.Extensions;
+using TheXDS.PhisherTank.Component;
+using TheXDS.PhisherTank.Models;
+
+namespace TheXDS.PhisherTank.Attacks;
+
+internal class CelebratorsPkAttack() : Attack("celebrators.pk")
+{
+    public override IEnumerable<AttackItem> GetAttacks(IAttackContext context)
+    {
+        yield return "";
+        yield return Form("cargando.php", GetCargandoForm);
+        var pin = MiscFaker.FakePin(6);
+        yield return Form("gege.php", faux => [("C1", pin)]);
+        yield return Form("gege.php", faux => [("C2", pin)]);
+        yield return Form("gege2.php", EmailPasswordForm("D1", "D2"));
+    }
+
+    private (string, string)[] GetCargandoForm(DataBase f)
+    {
+        return [
+            ("D2", f.Person.UserName),
+            ("D1", MiscFaker._rnd.CoinFlip() ? FauxData.GenerateRandomPassword() : f.Password),
+        ];
+    }
+}
