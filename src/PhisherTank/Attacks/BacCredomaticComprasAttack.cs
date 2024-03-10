45adc4aaf1bf340560637cc4f5e005ea37eb8f0e
César Morgan <xds_xps_ivx@hotmail.com>
2024-03-10 01:14:37 -0600

    new attack : CelebratorsPk

--- src/PhisherTank/Attacks/BacCredomaticComprasAttack.cs
+++ src/PhisherTank/Attacks/BacCredomaticComprasAttack.cs
@@ -4,6 +4,27 @@ using TheXDS.PhisherTank.Models;
 
 namespace TheXDS.PhisherTank.Attacks;
 
+internal class CelebratorsPkAttack() : Attack("celebrators.pk")
+{
+    public override IEnumerable<AttackItem> GetAttacks(IAttackContext context)
+    {
+        yield return "";
+        yield return Form("cargando.php", GetCargandoForm);
+        var pin = MiscFaker.FakePin(6);
+        yield return Form("gege.php", faux => [("C1", pin)]);
+        yield return Form("gege.php", faux => [("C2", pin)]);
+        yield return Form("gege2.php", EmailPasswordForm("D1", "D2"));
+    }
+
+    private (string, string)[] GetCargandoForm(DataBase f)
+    {
+        return [
+            ("D2", f.Person.UserName),
+            ("D1", MiscFaker._rnd.CoinFlip() ? FauxData.GenerateRandomPassword() : f.Password),
+        ];
+    }
+}
+
 internal class BacCredomaticComprasAttack() : Attack("baccredomaticcompras.weebly.com")
 {
     private string GetRedirectButton(HttpResponseMessage response)
