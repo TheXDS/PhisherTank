f3557ea34d79e0afd631486015aa811ab7b8ba10
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2024-07-20 00:23:53 -0600

    New Attack: Verificxar

--- src/PhisherTank/Attacks/HotSergu98Attack.cs
+++ src/PhisherTank/Attacks/HotSergu98Attack.cs
@@ -1,6 +1,4 @@
-﻿using TheXDS.PhisherTank.Component;
-using TheXDS.PhisherTank.Models;
-using TheXDS.PhisherTank.Attacks.Base;
+﻿using TheXDS.PhisherTank.Attacks.Base;
 
 namespace TheXDS.PhisherTank.Attacks;
 
